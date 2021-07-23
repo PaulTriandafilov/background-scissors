5f7ce02507c235fe93af3346462c369fb767c51f
sonic <sonic@wisebits.com>
2021-07-23 22:17:17 +0300

    undo test

--- components/UploadPhoto/DropModal.js
+++ components/UploadPhoto/DropModal.js
@@ -19,16 +19,10 @@ const DropModal = (props) => {
     const formData = new FormData();
     formData.append("file", file);
 
-    const response = await fetch(
-      "https://api.massless.io/v1/image/ml/remove-background",
-      {
-        method: "POST",
-        headers: {
-          "x-api-key": "RVu8i8KoBDRKRsYcbN3OusYXpt1Vzv1T5XCsaOl6CHx",
-        },
-        body: formData,
-      }
-    );
+    const response = await fetch("/api/rm-bg", {
+      method: "POST",
+      body: formData,
+    });
 
     if (response.status !== 200) {
       setHasErrors(true);
